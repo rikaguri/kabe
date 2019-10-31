1c71fb475daf4483c1791f12cc01504ff2d7a9dd
rikaguri <kirarika7@gmail.com>
2019-10-31 20:05:28 +0900

    kabe
    unity projectたたき台です

---
+++ kabe/Assets/_Scrips/blue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blue : MonoBehaviour
+{
+    public SerialHandler serialHandler;
+    public Text text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+text = this.GetComponent<Text>();
+text.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void OnDataReceived(string message)
+    {
+        try
+        {
+            string[] angles = message.Split(',');
+            //text.text = "x:" + angles[0] + "\n" + "y:" + angles[1] + "\n" + "z:" + angles[2] + "\n"; // シリアルの値をテキストに表示
+            text.text = "angles[0]";
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
---
+++ kabe/Assets/_Scrips/changemain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class changemain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("ChangeScene", 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+}
